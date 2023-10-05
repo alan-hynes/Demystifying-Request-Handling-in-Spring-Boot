d0c62e86d125613358fa35da1b120c585a98b65f
G00400498@atu.ie <g00400498@atu.ie>
2023-10-05 13:25:06 +0100

    Added subtraction method

--- src/main/java/ie/atu/demystifyingrequesthandlinginspringboot/CalculatorController.java
+++ src/main/java/ie/atu/demystifyingrequesthandlinginspringboot/CalculatorController.java
@@ -7,5 +7,9 @@ public class CalculatorController {
 
     @GetMapping
         public String add(@RequestParam int num1, int num2) {
-            return "Answer: " + num1 + num2; }
+            return "Answer: " + (num1 + num2); }
+
+    @GetMapping
+    public String subtract(@RequestParam int num1, int num2) {
+            return "Answer: " + (num1 - num2); }
 }
