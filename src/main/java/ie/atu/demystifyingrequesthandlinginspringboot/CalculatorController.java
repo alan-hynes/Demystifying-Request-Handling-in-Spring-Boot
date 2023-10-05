f7f6728ffaae7b988cd844d25a5754db159b1885
G00400498@atu.ie <g00400498@atu.ie>
2023-10-05 13:17:50 +0100

    Added calculator class and add method

--- src/main/java/ie/atu/demystifyingrequesthandlinginspringboot/CalculatorController.java
+++ src/main/java/ie/atu/demystifyingrequesthandlinginspringboot/CalculatorController.java
@@ -1,4 +1,11 @@
 package ie.atu.demystifyingrequesthandlinginspringboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 public class CalculatorController {
+
+    @GetMapping
+        public String add(@RequestParam int num1, int num2) {
+            return "Answer: " num1 + num2; }
 }
